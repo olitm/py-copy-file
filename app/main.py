6a5c0972fc3f954580aa70b26be5a232e53f4090
Ol <holmate@dontsp.am>
2025-05-18 19:43:10 +0300

    Added a solution for "Copy file"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+class FileNotFound():
+    """If file not found return"""
+
+
+def copy_file(command: str) -> None:
+    parts = command.strip().split()
+    if len(parts) != 3:
+        return
+    cmd, src, dest = parts
+    if cmd != "cp" or src == dest:
+        return
+    try:
+        with open(src, "r") as file_in, open(dest, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
